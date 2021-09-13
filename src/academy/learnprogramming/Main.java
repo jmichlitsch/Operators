644902f5d17dd2348db7ef59404abf2e35a5b5ed
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-09-13 11:25:43 -0600

    add if then operators

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -37,5 +37,22 @@ public class Main {
         //result = result / 3
         result /= 3;
         System.out.println(result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens.");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 }
