9eacf7db77419fc25cb79516423a16249ee5e53e
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-10-20 10:12:54 -0600

    add challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -50,9 +50,42 @@ public class Main {
         }
 
         int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
         }
 
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        // = is an assignment operator == is an equals to operator
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is not an error");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is supposed to happen.");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        double isX = 20.00d;
+        double isY = 80.00d;
+        double myValuesTotal = (isX + isY) * 100.00d;
+        System.out.println(myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println(theRemainder);
+
+        boolean isNoRemainer = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainer);
+        if (!isNoRemainer) {
+            System.out.println("Got some remainder");
+        }
     }
 }
